996acf6aff5fb808c4c9b7f516a6276c20bee8a8
Thibaut Moiroud <thib.moiroud@gmail.com>
2018-01-15 23:36:33 +0100

    Ajout écran de gestion des compétitions

--- ffn_site/Controllers/AdminController.cs
+++ ffn_site/Controllers/AdminController.cs
@@ -12,11 +12,23 @@ namespace ffn_site.Controllers
     [Authorize]
     public class AdminController : Controller
     {
+        private CategorieCompetitionDal catCompDal = new CategorieCompetitionDal();
+        private CategorieTourDal catTourDal = new CategorieTourDal();
+        private CategorieEpreuveDal catEpreuveDal = new CategorieEpreuveDal();
+        private CategorieBalletDal catBalletDal = new CategorieBalletDal();
+
         // GET: Admin
         public ActionResult Index()
         {
-            if (HttpContext.User.Identity.IsAuthenticated)
-                ViewBag.profil = new ProfilDal().getProfil(HttpContext.User.Identity.Name);
+            return View();
+        }
+
+        public ActionResult Competition()
+        {
+            ViewBag.catsComp = catCompDal.GetAll();
+            ViewBag.catsTour = catTourDal.GetAll();
+            ViewBag.catsEpreuve = catEpreuveDal.GetAll();
+            ViewBag.catsBallet = catBalletDal.GetAll();
             return View();
         }
     }
